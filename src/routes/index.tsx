5a2ca7468fe7e1f264ce60263b4f8d6519708f63
João Vítor Batistella <joaovitor.batistella.ifrs@gmail.com>
2020-05-28 22:07:38 -0300

    folders fixed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
-import Show from '../pages/Show';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/show/:show+" component={Show} />
   </Switch>
 );
 
